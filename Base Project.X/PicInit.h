cd2eb764aa8c5a68a6635b3de337209377abc4b5
Gabriel Peal <gpeal@u.northwestern.edu>
2012-01-17 16:37:45 -0600

    moved picinit to the right place

--- Base Project.X/PicInit.h
+++ Base Project.X/PicInit.h
@@ -1,14 +1,10 @@
-#ifndef PICINIT_H
+#ifndef PICINT_H
 #define PICINIT_H
 
 #define SYS_FREQ 80000000
 
 void initialize();
-void initializePic();
-void initializePorts();
-void initializeTimers();
-void initializeUart();
 
-int setupTimer(int timer, int frequency, int priority);
+void initializePic();
 
 #endif
