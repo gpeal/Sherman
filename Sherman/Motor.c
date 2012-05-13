97a9cb29bebe83740bf9dc2ae2733345ecd57b68
gpeal <gpeal@u.northwestern.edu>
2012-05-12 22:28:57 -0500

    Feature: new untested motor driver

--- Sherman/Motor.c
+++ Sherman/Motor.c
@@ -5,17 +5,44 @@ void setupMotor(int motor)
     setupPWM(motor);
 }
 
-void setMotor(int motor, int dutyCycle, int direction)
+void setMotor(int motor, int speed, int direction)
 {
-    int motorOc;
+    int motorOc, control1, control2;
+
+    switch(direction)
+    {
+        case MOTOR_DIRECTION_RIGHT:
+            control1 = 1;
+            control2 = 0;
+            break;
+        case MOTOR_DIRECTION_LEFT:
+            control1 = 0;
+            control2 = 1;
+            break;
+        case MOTOR_DIRECTION_BRAKE:
+            control1 = 1;
+            control2 = 1;
+            break;
+        default:
+            control1 = 1;
+            control2 = 1;
+            break;
+    }
+
     switch(motor)
     {
         case MOTOR_WHEEL_LEFT:
             motorOc = MOTOR_WHEEL_LEFT_OC;
-            MOTOR_WHEEL_LEFT_DIRECTION = direction;
+            MOTOR_WHEEL_LEFT_CONTROL1 = control1;
+            MOTOR_WHEEL_LEFT_CONTROL2 = control2;
+            break;
+        case MOTOR_WHEEL_RIGHT:
+            motorOc = MOTOR_WHEEL_RIGHT_OC;
+            MOTOR_WHEEL_RIGHT_CONTROL1 = control1;
+            MOTOR_WHEEL_RIGHT_CONTROL2 = control2;
             break;
         default:
             return;
     }
-    setDutyCycle(motorOc, dutyCycle);
+    setDutyCycle(motorOc, speed);
 }
--- Sherman/Motor.h
+++ Sherman/Motor.h
@@ -4,6 +4,10 @@
 #include "Components.h"
 #include "PWM.h"
 
+#define MOTOR_DIRECTION_LEFT 0
+#define MOTOR_DIRECTION_RIGHT 1
+#define MOTOR_DIRECTION_BRAKE -1
+
 
 void setupMotor(int motor);
 void setMotor(int motor, int dutyCycle, int direction);
